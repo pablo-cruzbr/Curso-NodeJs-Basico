c811c42f16b45d101f3dc4c13a37f6bf03f063bc
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-12 12:03:17 -0300

    Path:  Nome do Diretório atual

--- modules/path.js
+++ modules/path.js
@@ -1,4 +1,9 @@
+const path = require('path');
 const patch = require('path')
 
 //Apenas o nome do arquivo atual
 console.log(patch.basename(__filename));
+
+
+//Nome do Diretório atual
+console.log(path.dirname(__filename))

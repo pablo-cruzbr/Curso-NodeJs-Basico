e92aa785fde253db1b83c7082396e8b3f4739e92
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-12 14:27:28 -0300

    Path: Criamos um objeto path com várias informações
    root, dir, base, ext, name do arquivo

--- modules/path.js
+++ modules/path.js
@@ -6,7 +6,13 @@ console.log(patch.basename(__filename));
 
 
 //Nome do Diretório atual
-console.log(path.dirname(__filename))
+console.log(path.dirname(__filename));
 
 //Extensão do arquivo 
-console.log(path.extname(__filename))
+console.log(path.extname(__filename));
+
+//Criar um objeto path
+console.log(path.parse(__filename));
+
+
+

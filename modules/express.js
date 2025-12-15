039632a9fca42dff4ee750348b810fbae710448e
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-15 14:44:42 -0300

    Criando endpoints get, patch e delete

--- modules/express.js
+++ modules/express.js
@@ -41,6 +41,70 @@ app.get('/getusers', async (req, res) => {
     }
 });
 
+app.get('/getusers/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const user = await UserModel.findById(id);
+        
+        res.status(201).json(user);
+        
+    } catch (error) { 
+    
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({
+                message: 'Falha na validação: dados obrigatórios faltando.',
+                errors: error.errors
+            });
+        }
+        
+        res.status(500).send(error.message);
+    }
+});
+
+
+app.patch('/updateuser/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+        
+        res.status(201).json(user);
+        
+    } catch (error) { 
+    
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({
+                message: 'Falha na validação: dados obrigatórios faltando.',
+                errors: error.errors
+            });
+        }
+        
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete('/deleteuser/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const deletedUser = await UserModel.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'Usuário não encontrado.' });
+        }
+        
+        return res.status(204).send(); 
+
+    } catch (error) { 
+        
+        if (error.kind === 'ObjectId') {
+             return res.status(400).json({ message: 'O ID fornecido não está no formato correto.' });
+        }
+        
+        res.status(500).send(error.message);
+    }
+});
 
 app.post('/users', async (req, res) => {
     try {

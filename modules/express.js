af2be828d020d4d43f2a99cbf1c01f9a67cce4b9
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-15 10:45:35 -0300

    JSON criado e enviado
    rota get "/users"

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,20 @@ app.get('/home', (req, res) => {
     res.status(200).send('<h1>hello word</h1>')
 });
 
+app.get("/users", (req, res)=> {
+    const users = [
+        {
+            name: "Pablo Cruz",
+            email: "pablo.cruz@gmail.com",
+        },
+        {
+            name: "Geovanna Silva",
+            email: "geovanna.silva@gmail.com",
+        }
+    ]
+    res.status(200).json(users);
+})
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com Express na porta: ${port}`))

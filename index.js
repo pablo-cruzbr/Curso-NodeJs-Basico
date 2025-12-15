a91e7984d00ccf2acdaa4c7b6c7463b6791a0f3a
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-15 11:11:08 -0300

    configuração com banco de dados realizada com sucesso!

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const {Person} = require("./person");
 const dotenv = require("dotenv");
-
+const connectToDataBase = require('./src/database/connect')
 dotenv.config();
+
+connectToDataBase();
+
 
 //require('./modules/path')
 //require('./modules/fs')
 //require('./modules/http');
 
-require("./modules/express")
+//require("./modules/express")
 
 //Executar: node index.js
